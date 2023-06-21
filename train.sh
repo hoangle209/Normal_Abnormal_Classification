6bf9c3eb22096b92f3f63963f098b1b41ba92cb8
MinhHoangLe <le.minh.hoang@mqsolutions.com.vn>
2023-06-21 13:27:40 +0700

    add git ignore

--- train.sh
+++ train.sh
@@ -1,1 +1,7 @@
-python train.py 
+python train.py --path "home/mq/data_disk2T/hieu/hoang/data_set/train" \
+                --val_path "home/mq/data_disk2T/hieu/hoang/data_set/val" \
+                --input_h 416 \
+                --input_w 416 \
+                --lr 1.25e-3 \
+                --label-smoothing \
+                --val 1
